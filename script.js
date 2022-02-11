1cf7c9f613742b8ef2b83674de29c303688c2751
Elise <noctiphobic@gmail.com>
2022-02-10 19:16:44 -0600

    clearGrid() working, and default grid fxn working, but input number grid is creating a grid that is a multiple of 10

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
-//input form for usesr to choose grid size
-const numberInputValue = document.getElementById('numInput').value;
+//input form for user to choose grid size
 const button = document.getElementById('submit');
-const numberInput = document.getElementById('number'); 
+const number = document.getElementById('numInput'); 
 
-/*button.addEventListener('click', () => {
-	makeGrid(e);
-});
-this also works! But I prefer the one below for brevity. 
-I'd use the one above if I had more than one parameter
-Because it's a function inside a function. It will 
-evaluate the inside function first, but it won't have received 
-the event data that comes from the event listener fxn first
-*/
-
-//one of the mistakes I made below was not realizing the e itself is the value. no need to do e.value
-button.addEventListener('click', makeGrid);
+/////////////////////////MAIN////////////////////
 //default grid created
-makeGrid(16);
+defaultG(16);
 
-function makeGrid(e) {
-	var num = e;
-	//make a container row for num
-//each container row has num cells in it
+//make grid chosen from number input
+button.addEventListener('click', (e) => {
+	const num = number.value.toString();
+	console.log(num);
+	makeGrid(num);
+});
+///////////////////////////////////////////////
+
+//WANT THE VALUE OF THE EVENT THAT TRIGGERED THE EVENT, NOT THE EVENT ITSELF 
+
+function defaultG(num) {
+	for (let i=1;i<num+1;i++) {
+                var container = document.querySelector('#container');
+                var divCol = document.createElement('div');
+
+                let concatWord = 'column';
+                let concatNum = i.toString();
+                let idName = concatWord.concat(concatNum);
+
+                divCol.setAttribute('id',`${idName}`);
+                divCol.setAttribute('class','column');
+
+                container.appendChild(divCol);
+
+                //now append num new div nodes to that node just created
+                for(let j=1;j<num+1;j++) {
+                        var divRow = document.createElement('div');
+                        
+                        concatNum = j.toString();
+                        concatWord = 'row';
+                        idName = concatWord.concat(concatNum);
+
+                        divRow.setAttribute('id',`${idName}`);
+                        divRow.setAttribute('class','row');
+                        divCol.appendChild(divRow);
+                }
+        }
+} 
+
+function makeGrid(num) {
+	console.log(num);
+	clearGrid();
 	//make the divs for each row
 	for (let i=1;i<num+1;i++) {
+		console.log(num);
 		var container = document.querySelector('#container');
 		var divCol = document.createElement('div');
 
@@ -47,33 +74,20 @@ function makeGrid(e) {
 			divRow.setAttribute('id',`${idName}`);
 			divRow.setAttribute('class','row');
 			divCol.appendChild(divRow);
-
                 }
-
-
 	}
 }
 
+///////////////////////////////////////////////////////////////////////////////////////
 function clearGrid() {
-	var container = document.getElementById('container');
-	const inputDiv = container.removeChild(container.firstChild);
-	const label = container.removeChild(container.firstChild);
-	const number = container.removeChild(container.firstChild);
-	//const submit = container.removeChild(container.firstChild);
-
-	while (container.firstChild) {
-		container.removeChild(container.firstChild);
-	}
-	
-	container.appendChild(inputDiv);
-	container.appendChild(label);
-	container.appendChild(number);
-	//container.appendChild(submit);
+	document.querySelectorAll('.column').forEach(e => e.remove());
 }
 
-////hover event that changes div colors? and then do they need to change back? no, that doesn't make sense
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////hover event that changes div colors
 const rows = document.querySelectorAll('.row');
 const columns = document.querySelectorAll('.column');
+
 
 rows.forEach((row) => {
 	row.addEventListener('mouseover', () => {
